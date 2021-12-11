0ac10e3d5abdd8f5d3f5782cdaf823e31c35a96f
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-10 22:22:45 -0500

    add button style

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,6 +24,7 @@ export default function Home() {
             lubricantes, accesorios y más.</p>
 
         </div>
+        <button>Contáctanos</button>
       </section>
       <section id='miedos'>
 
