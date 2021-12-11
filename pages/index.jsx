d2845ccbfbaa5add31bc190b8ff15ad9777a873f
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-10 22:24:07 -0500

    fix build

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.scss'
+import styles from '../styles/Home.module.scss'
 import NavBar from './shared/navbar'
 
 export default function Home() {
