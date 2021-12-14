291f0a00c77c073eb73b7fbd9e47a2605e078a5c
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-14 00:49:41 -0500

    add terminos link

--- pages/shared/footer.jsx
+++ pages/shared/footer.jsx
@@ -1,12 +1,21 @@
+import Link from 'next/link'
 import styles from '../../styles/Footer.module.scss'
-
 export default function Footer() {
     return (
         <div className={styles.footer}>
             <div className={styles.container}>
                 <div className={styles.links}>
-                    <p>Política de privacidad</p>
-                    <p id={styles.last}>Términos y condiciones</p>
+
+                    <Link href={'/politica-y-privacidad'}>
+                        <a>
+                            <p>Política de privacidad</p>
+                        </a>
+                    </Link>
+                    <Link href={'/terminos-y-condiciones'}>
+                        <a>
+                            <p id={styles.last}>Términos y condiciones</p>
+                        </a>
+                    </Link>
 
                 </div>
                 <div className={styles.copyright}>
